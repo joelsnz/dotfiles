eb03e9cfbf7a7ad1ed407ae2095de60c58ae039b
joelsnz <joel04slm@gmail.com>
2024-03-10 20:41:50 +0100

    Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,17 +14,4 @@ starship init fish | source
 
 pyenv init - | source
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/joel/.config/miniconda3/bin/conda
-    eval /home/joel/.config/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/joel/.config/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/joel/.config/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/joel/.config/miniconda3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
 zoxide init fish | source

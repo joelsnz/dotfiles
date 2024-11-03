b1f92fc8b13a712b64517b19693b821a3f345c71
joelsnz <joel04slm@gmail.com>
2024-11-03 11:42:08 +0100

    Set default latex compiler vimtex.lua

--- nvim/.config/nvim/lua/joel/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/joel/plugins/vimtex.lua
@@ -3,5 +3,8 @@
 return {
 	"lervag/vimtex",
 	lazy = false,
-	init = function() end,
+	init = function()
+		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_compiler_method = "tectonic"
+	end,
 }

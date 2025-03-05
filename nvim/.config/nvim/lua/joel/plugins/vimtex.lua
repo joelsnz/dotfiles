8324a3999c3953c190340bb65c43ffe747fdda07
joelsnz <joel04slm@gmail.com>
2025-03-05 09:38:12 +0100

    hypr nvim zsh

--- nvim/.config/nvim/lua/joel/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/joel/plugins/vimtex.lua
@@ -5,6 +5,6 @@ return {
 	lazy = false,
 	init = function()
 		vim.g.vimtex_view_method = "zathura"
-		vim.g.vimtex_compiler_method = "tectonic"
+		vim.g.vimtex_compiler_method = "latexmk"
 	end,
 }

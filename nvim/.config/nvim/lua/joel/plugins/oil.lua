1423b653b98bf6131a17fc018e1fad57007cf3bc
Joel Sanz Martí <joel04slm@gmail.com>
2025-09-09 11:07:44 +0200

    make oil.nvim show hidden file

--- nvim/.config/nvim/lua/joel/plugins/oil.lua
+++ nvim/.config/nvim/lua/joel/plugins/oil.lua
@@ -8,4 +8,18 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function()
+    local oil = require("oil")
+    oil.setup({
+      view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+      -- This function defines what is considered a "hidden" file
+      is_hidden_file = function(name, bufnr)
+        local m = name:match("^%.")
+        return m ~= nil
+      end
+      }
+    })
+  end
 }

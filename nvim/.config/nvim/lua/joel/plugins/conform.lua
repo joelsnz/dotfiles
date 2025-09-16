b10851f5104b04220c8ccb8f7c9f2fc2b8da6468
Joel Sanz Martí <joel04slm@gmail.com>
2025-09-16 10:46:14 +0200

    Add plugin name before descs

--- nvim/.config/nvim/lua/joel/plugins/conform.lua
+++ nvim/.config/nvim/lua/joel/plugins/conform.lua
@@ -8,18 +8,18 @@ return {
 		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				c = { "clang-format" }
+				c = { "clang-format" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000
+				timeout_ms = 1000,
 			},
 			formatters = {
 				clang_format = {
-					prepend_args = { "--style=file" }
-				}
-			}
+					prepend_args = { "--style=file" },
+				},
+			},
 		})
-  end
+	end,
 }
--- nvim/.config/nvim/lua/joel/plugins/oil.lua
+++ nvim/.config/nvim/lua/joel/plugins/oil.lua
@@ -1,25 +1,18 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
-  config = function()
-    local oil = require("oil")
-    oil.setup({
-      view_options = {
-      -- Show files and directories that start with "."
-      show_hidden = true,
-      -- This function defines what is considered a "hidden" file
-      is_hidden_file = function(name, bufnr)
-        local m = name:match("^%.")
-        return m ~= nil
-      end
-      }
-    })
-  end
+	"stevearc/oil.nvim",
+	opts = {},
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	lazy = false,
+	config = function()
+		local oil = require("oil")
+		oil.setup({
+			view_options = {
+				show_hidden = true,
+				is_hidden_file = function(name, bufnr)
+					local m = name:match("^%.")
+					return m ~= nil
+				end,
+			},
+		})
+	end,
 }

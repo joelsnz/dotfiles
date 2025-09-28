f804096af5d1c9162de222503dda9bc811bbcd03
joelsnz <joel04slm@gmail.com>
2025-09-28 18:43:03 +0200

    correct merge

--- nvim/.config/nvim/lua/joel/plugins/oil.lua
+++ nvim/.config/nvim/lua/joel/plugins/oil.lua
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
--- file explorer
-return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
-=======
 return {
 	"stevearc/oil.nvim",
 	opts = {},
@@ -28,5 +15,4 @@ return {
 			},
 		})
 	end,
->>>>>>> b10851f5104b04220c8ccb8f7c9f2fc2b8da6468
 }
